3f3a3733b4a2c134f0319ad66831df74cc349d1a
prakhars0206 <prakharsedg@outlook.com>
2026-01-17 19:14:05 +0000

    revert

--- backend/src/main/java/ILP/coursework/ILP/coursework1/dto/DeliveryPathResponse.java
+++ backend/src/main/java/ILP/coursework/ILP/coursework1/dto/DeliveryPathResponse.java
@@ -3,10 +3,8 @@ package ILP.coursework.ILP.coursework1.dto;
 import ILP.coursework.ILP.coursework1.dto.JsonDtos.Position;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record DeliveryPathResponse(
-        @JsonProperty("cost") double totalCost,
+        double totalCost,
         int totalMoves,
         List<DronePath> dronePaths
 ) {
